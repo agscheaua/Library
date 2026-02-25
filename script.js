1de83582fcc35ed46e72e184fb7b61fcf5a94c8a
Alex <agscheaua@gmail.com>
2026-02-25 22:58:15 +0200

    i finish the layout of new book button and i did the logic for a new book card

--- script.js
+++ script.js
@@ -25,30 +25,67 @@ function addBookToLibrary(title, author, pages, status) {
 
 // created books;
 
-addBookToLibrary("theHobbit", "JJJ Tolkien", 346, "not read it");
-addBookToLibrary("theHobbit", "JJJ Tolkien", 346, "read it");
-addBookToLibrary("theHobbit", "JJJ Tolkien", 346, "not read it");
-addBookToLibrary("theHobbit", "JJJ Tolkien", 346, "read it");
+addBookToLibrary("theHobbit", "JJJ Tolkien", 346, "I did not read it");
+addBookToLibrary("theHobbit", "JJJ Tolkien", 346, "I read it");
 console.log(myLibrary)
 
-// create the book cards;
+// create the book cards; 
 
 myLibrary.forEach( (book) => {
     const bookCardContainer = document.querySelector(".bookCardContainer");
     const newDiv = document.createElement("div")
     newDiv.classList.add("book");
     bookCardContainer.appendChild(newDiv);
-    newDiv.textContent = book.title + ", written by " +book.author + ", has " + book.pages + " pages, and I did " + book.status + ".";
+    newDiv.textContent = book.title + ", written by " +book.author + ", has " + book.pages + " pages, and " + book.status + ".";
 } );
 
-// dialog window 
+// dialog window; 
 
 const dialogShow = document.querySelector(".dialogShow");
 const dialogWindow = document.querySelector(".dialogWindow");
 const dialogClose = document.querySelector(".dialogClose");
 dialogShow.addEventListener("click", () => {
     dialogWindow.showModal();
+    infoMessage.textContent = "";
 });
 dialogClose.addEventListener("click", () => {
-    dialogWindow.close();   
+    dialogWindow.close();
+    bookName.value = "";
+    author.value = "";
+    pageNr.value = "";
+    statusBook.value = "";   
+    infoMessage.textContent = "";
 });
+
+// create new book cards;
+
+const bookName = document.querySelector("#bookName");
+const author = document.querySelector("#author");
+const pageNr = document.querySelector("#pageNr");
+const statusBook = document.querySelector("#statusBook");
+const infoMessage = document.querySelector(".infoMessage");
+const submitForm = document.querySelector(".submitForm");
+
+submitForm.addEventListener("click", (elem) => { 
+    elem.preventDefault();
+    if (bookName.value && author.value && pageNr.value && statusBook.value) {
+        addBookToLibrary(bookName.value, author.value, pageNr.value, statusBook.value);
+        const bookCardContainer = document.querySelector(".bookCardContainer");
+        const newDiv = document.createElement("div")
+        newDiv.classList.add("book");
+        bookCardContainer.appendChild(newDiv);
+        newDiv.textContent = bookName.value + ", written by " + author.value + ", has " + pageNr.value + " pages, and " + statusBook.value + ".";
+        bookName.value = "";
+        author.value = "";
+        pageNr.value = "";
+        statusBook.value = "";
+    }
+    else {
+        infoMessage.textContent = "You didn't fill in all the forms!";
+        bookName.value = "";
+        author.value = "";
+        pageNr.value = "";
+        statusBook.value = "";
+    };
+}) 
+
