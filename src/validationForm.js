1c5472ff0417d5691c0fbb6ce15e846ac4ab9e79
Alex <agscheaua@gmail.com>
2026-05-12 23:04:21 +0300

    most of the functionlity is done i need to create a regex that specify a pattern for nr only that do not start with 0

--- src/validationForm.js
+++ src/validationForm.js
@@ -4,14 +4,17 @@ export {addClickVerification};
 export {clearErrorBoxWhenCloseModal};
 export {clearErrorBoxWhenOpenModal};
 
+// get all the necesary elements to assign them different actions;
+
+
 const bookName = document.querySelector('#bookName');
 const author = document.querySelector('#author');
 const pageNr = document.querySelector('#pageNr');
 const statusBook = document.querySelector('#statusBook');
 
-const infoMessage = document.querySelector('.infoMessage');
 const submitForm = document.querySelector('.submitForm');
-const bookCardContainer = document.querySelector('.bookCardContainer');
+
+// hide all error boxes to not be seen when we open the modal;
 
 function hideErrorBox() {
   const getAllErrorBox = document.querySelectorAll(".errorMsj");
@@ -21,11 +24,15 @@ function hideErrorBox() {
   }); 
 };
 
+// functions to set the the text and style it when the control do not meet the; 
+// requirements;
+
 function checkValidityBookName() {
   const errorMessageBook = document.querySelector(".errorMessageBook");
   if ( !(bookName.checkValidity()) ) {
     errorMessageBook.textContent = "Book name is empty.";
     errorMessageBook.classList.remove("hideError");
+    errorMessageBook.classList.add("error");
   } else {
     errorMessageBook.textContent = "";
     errorMessageBook.classList.add("hideError");
@@ -37,6 +44,7 @@ function checkValidityAuthor() {
   if ( !(author.checkValidity()) ) {
     errorMessageAuthor.textContent = "Author name is empty.";
     errorMessageAuthor.classList.remove("hideError");
+    errorMessageAuthor.classList.add("error");
   } else {
     errorMessageAuthor.textContent = "";
     errorMessageAuthor.classList.add("hideError");
@@ -44,10 +52,16 @@ function checkValidityAuthor() {
 };
 
 function checkValidityPageNr() {
+  const regexNrOnly = new RegExp("^[0-9]");
   const errorMessagePage = document.querySelector(".errorMessagePage");
-  if ( !(pageNr.checkValidity()) ) {
+  if ( !(regexNrOnly.test(pageNr.value)) ) {
+    errorMessagePage.textContent = "Invalid value inputed, only numbers between 0-9";
+    errorMessagePage.classList.remove("hideError");
+    errorMessagePage.classList.add("error");
+  } else if ( !(pageNr.checkValidity()) ) {
     errorMessagePage.textContent = "Page number is empty.";
     errorMessagePage.classList.remove("hideError");
+    errorMessagePage.classList.add("error");
   } else {
     errorMessagePage.textContent = "";
     errorMessagePage.classList.add("hideError");
@@ -59,11 +73,14 @@ function checkValidityStatusBook() {
   if ( !(statusBook.checkValidity()) ) {
     errorMessageStatus.textContent = "Status is empty.";
     errorMessageStatus.classList.remove("hideError");
+    errorMessageStatus.classList.add("error");
   } else {
     errorMessageStatus.textContent = "";
     errorMessageStatus.classList.add("hideError");
   };
 };
+
+// add the checkingValidation function as events for the submit form modal button;
 
 function addClickVerification() {
   submitForm.addEventListener('click', checkValidityBookName);
@@ -71,6 +88,8 @@ function addClickVerification() {
   submitForm.addEventListener("click", checkValidityPageNr);
   submitForm.addEventListener("click", checkValidityStatusBook);
 };
+
+// clear modal of text when closeing and opening;
 
 const dialogShow = document.querySelector('.dialogShow');
 const dialogWindow = document.querySelector('.dialogWindow');
